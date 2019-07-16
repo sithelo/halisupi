31892e1c8dc7dfc44171fae5954bd550592147be
sithelo <sithelo@gmail.com>
2019-07-16 16:12:47 +0200

    homepage holder

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import layout from '../components/layout';
 import styles from '../css/error.module.css';
-// import Banner from "../components/Banner"
+import banner from '../components/banner';
 import AniLink from "gatsby-plugin-transition-link/AniLink"
 // import SEO from "../components/SEO"
 export default function error() {
@@ -9,11 +9,11 @@ export default function error() {
     <layout>
       {/* <SEO title="Error" /> */}
       <header className={styles.error}>
-        {/* <Banner title="oops it's a dead end"> */}
+        <banner title="oops it's a dead end">
           <AniLink fade to="/" className="btn-white">
             back to home page
           </AniLink>
-        {/* </Banner> */}
+        </banner>
       </header>
     </layout>
   )
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,17 @@
 import React from "react";
+import AniLink from 'gatsby-plugin-transition-link/AniLink';
+import styles from '../css/error.module.css';
 import layout from '../components/layout';
-
-export default () => <layout>... under construstion</layout>
+import banner from '../components/banner';
+export default () => <layout>
+    <header className={styles.error}>
+        <banner 
+            title="continue exploring"
+            info=" Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, officiis."
+        >
+          <AniLink fade to="/404" className="btn-white">
+          explore halisupi
+          </AniLink>
+        </banner>
+    </header>
+</layout>
