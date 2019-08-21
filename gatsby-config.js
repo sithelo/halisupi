c955b14f3b36165c4e0a0e01e246c6a773bd9344
sithelo <sithelo@gmail.com>
2019-08-21 14:47:48 +0200

    added the services and about component

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,11 +3,23 @@
  *
  * See: https://www.gatsbyjs.org/docs/gatsby-config/
  */
-
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+const path = require(`path`) 
 module.exports = {
   /* Your site config here */
   plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: path.join(__dirname, `src`, `images`),
+      },
+    },
     `gatsby-plugin-sass`,
     `gatsby-plugin-transition-link`,
+    `gatsby-transformer-sharp`, 
+    `gatsby-plugin-sharp`
   ],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,38 @@
-import React from "react";
-import AniLink from 'gatsby-plugin-transition-link/AniLink';
-import styles from '../css/error.module.css';
-import Layout from '../components/layout';
-import Banner from '../components/banner';
-export default () => <Layout>
-    <header className={styles.error}>
-        <Banner 
-            title="continue exploring"
-            info="Website still under construction...."
+import React, { memo } from "react";
+import { graphql } from "gatsby"
+import AniLink from "gatsby-plugin-transition-link/AniLink";
+import Layout from "../components/layout";
+import Banner from "../components/banner";
+import About from "../components/home/about";
+import Services from "../components/home/services";
+import StyledHeader from "../components/layout/styledheader";
+export default memo(({ data }) => {
+    console.log('Data ', data['bg']);
+  return (
+    <Layout>
+      <StyledHeader home="true" img={data.bg.childImageSharp.fluid}>
+        <Banner
+          title="continue exploring"
+          info="Website still under construction...."
         >
           <AniLink fade to="/404" className="btn-white">
-          explore halisupi
+            explore halisupi
           </AniLink>
         </Banner>
-    </header>
-</Layout>
+      </StyledHeader>
+      <About />
+      <Services />
+    </Layout>
+  )
+})
+export const query = graphql`
+  {
+    bg: file(relativePath: { eq: "bg.jpg" }) {
+      childImageSharp {
+        fluid(quality: 90, maxWidth: 4160) {
+          ...GatsbyImageSharpFluid_withWebp
+        }
+      }
+    }
+  }
+`
